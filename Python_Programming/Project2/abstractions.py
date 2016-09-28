4e5c28a16309de99179751db5b322465c51d26c0
Nathan Choate <ndchoate@gmail.com>
2016-09-28 13:16:18 -0400

    Added reviews attribute to restaurant abstraction, implemented restaurant_ratings method.

--- Python_Programming/Project2/abstractions.py
+++ Python_Programming/Project2/abstractions.py
@@ -64,13 +64,14 @@ def make_restaurant(name, location, categories, price, reviews):
     # You may change this starter implementation however you wish, including
     # adding more items to the dictionary below.
     # BEGIN Question 1
-    "*** REPLACE THIS LINE ***"
+
     # END Question 1
     return {
         'name': name,
         'location': location,
         'categories': categories,
         'price': price,
+        'reviews': reviews
     }
 
 def restaurant_name(restaurant):
@@ -94,7 +95,7 @@ def restaurant_ratings(restaurant):
     """Return a list of ratings, which are numbers from 1 to 5, of the
     restaurant based on the reviews of the restaurant."""
     # BEGIN Question 1
-    "*** REPLACE THIS LINE ***"
+    return restaurant['reviews']
     # END Question 1
 
 
